ad132f9a80ffa103f0fbbf0465fee51577e2b782
Ruhul Amin <ruhulamin005@users.noreply.github.com>
2020-09-07 20:45:03 +0600

    included xml section

---
+++ xmlWrite.php
@@ -0,0 +1,29 @@
+<?php
+
+if($_GET['name']!= ''&& $_GET['username']!= ''&& $_GET['password']!= ''
+&& $_GET['retypepassword']!= ''&&  $_GET['gender']!= ''&&
+ $_GET['programmingskills']!= ''&&  $_GET['contactno']!= ''&& $_GET['email'] &&
+ $_GET['college']!= '')
+ {
+    
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
